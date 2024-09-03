3afd0e3d9489d487579321a2515330cc611a7fa8
“mattcullenmeyer” <mattcullenmeyer@gmail.com>
2024-09-03 17:09:56 -0600

    added datadog sdk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { datadogRumSdk } from "@/libs/datadogRumSdk";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+    <Head>
+      <script dangerouslySetInnerHTML={{ __html: datadogRumSdk }} />
+    </Head>
+    
+    <Component {...pageProps} />
+  </>
+  )
 }
